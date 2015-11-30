b9afb22a55b0d16e5f3f481e6486ce074d2bf703
Cheng Kuan <ck@codeck.org>
2015-11-30 15:29:46 +0800

    refactorized legacy strkey

--- shared/src/main/scala/legacy_strkey.scala
+++ shared/src/main/scala/legacy_strkey.scala
@@ -3,17 +3,18 @@ package org.strllar.stellarbase
 package legacy {
 
 import org.strllar.stellarbase.{RipeMD160, Sha256}
+import util.{Try, Failure, Success}
 
 object StrKey {
   object VersionEncoding
   {
-    val VER_NONE = 1
-    val VER_NODE_PUBLIC = 122  // n
-    val VER_NODE_PRIVATE = 102 //h
-    val VER_ACCOUNT_ID = 0 // g
-    val VER_ACCOUNT_PUBLIC = 67 //p
-    val VER_ACCOUNT_PRIVATE = 101 //h
-    val VER_SEED = 33 // s
+    val VER_NONE :Byte = 1
+    val VER_NODE_PUBLIC :Byte = 122  // n
+    val VER_NODE_PRIVATE :Byte = 102 //h
+    val VER_ACCOUNT_ID :Byte = 0 // g
+    val VER_ACCOUNT_PUBLIC :Byte = 67 //p
+    val VER_ACCOUNT_PRIVATE :Byte = 101 //h
+    val VER_SEED :Byte = 33 // s
   } ;
   val alphabet = "gsphnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCr65jkm8oFqi1tuvAxyz"
   val revalphabet = alphabet.zipWithIndex.toMap
@@ -38,27 +39,27 @@ object StrKey {
     Vector.fill(chars.takeWhile(_ == alphabet.charAt(0)).length)(0.toByte) ++ bn.toByteArray
   }
 
-  def SHA256(payload :IndexedSeq[Byte]) :Vector[Byte] = {
+  private[this] def SHA256(payload :IndexedSeq[Byte]) :Vector[Byte] = {
     val buff = payload.toArray
     val md256 = new Sha256()
     md256.update(buff)
     Vector(md256.digest:_*)
   }
-  def SHA256Hash(payload :IndexedSeq[Byte]) :Vector[Byte] = {
+  private[this] def SHA256Hash(payload :IndexedSeq[Byte]) :Vector[Byte] = {
     //SHA256Hash: double sha256
     val hash256 = SHA256(payload).toArray
     val md256 = new Sha256()
     md256.update(hash256)
     Vector(md256.digest:_*)
   }
-  def Hash160(payload :IndexedSeq[Byte]) :Vector[Byte] = {
+  private[this] def Hash160(payload :IndexedSeq[Byte]) :Vector[Byte] = {
     val hash256 = SHA256(payload).toArray
     val md160 = new RipeMD160
     md160.update(hash256)
     Vector(md160.digest:_*)
   }
 
-  def encodeCheck(payload :IndexedSeq[Byte], version :Byte) :String = {
+  private[this] def encodeCheck(payload :IndexedSeq[Byte], version :Byte) :String = {
     val buff = new Array[Byte](1 + payload.length + 4)
     buff(0) = version
     payload.copyToArray(buff, 1)
@@ -69,32 +70,57 @@ object StrKey {
     Base58en(buff)
   }
 
-  def decodeCheck(chars :String) :Vector[Byte] = {
-    val bytes = Base58de(chars)
-    val ver = bytes.head
-    val body = bytes.dropRight(4)
-    val checksum = bytes.takeRight(4)
+  private[this] def decodeCheck(chars :String, demandver :Byte) :Try[Vector[Byte]] = {
+    Try(Base58de(chars)).flatMap((bytes) => {
+      val ver = bytes.head
+      val body = bytes.dropRight(4)
+      val checksum = bytes.takeRight(4)
+      val hash4 = SHA256Hash(body).take(4)
 
-    val hash4 = SHA256Hash(body).take(4)
-    assert(hash4 == checksum) //TODO
-    body.tail //payload
+      if (ver != demandver) Failure(new Exception("wrong version"))
+      else if (hash4 != checksum) Failure(new Exception("wrong crc"))
+      else Success(body.tail) //payload
+    })
   }
 
   def seed(bytes :Array[Byte]) = {
-    encodeCheck(bytes, VersionEncoding.VER_SEED.toByte)
+    encodeCheck(bytes, VersionEncoding.VER_SEED)
   }
 
   def address(pubkey :Array[Byte]) = {
     val accountid = Hash160(pubkey)
-    encodeCheck(accountid, VersionEncoding.VER_ACCOUNT_ID.toByte)
+    encodeCheck(accountid, VersionEncoding.VER_ACCOUNT_ID)
   }
 
-  def parseSeed(skey :String): Unit = {
+  object Converters {
+    import org.strllar.stellarbase.{StrSeed, StrAddress}
 
-  }
+    implicit def str2keys(rawstr :String) = {
+      new {
+        def asLegacySeed = new {
+          private[this] val tmpseed = decodeCheck(rawstr, VersionEncoding.VER_SEED).get
+          def upgrade = new StrSeed(tmpseed toArray) {
+            def legacy = seed(rawseed toArray)
+          }
+          override def toString = seed(tmpseed toArray)
+        }
 
-  def parseAddres(gkey :String): Unit ={
+        def asLegacyAddress = new {
+          val rawhash160 = decodeCheck(rawstr, VersionEncoding.VER_ACCOUNT_ID).get
+          override def toString = encodeCheck(rawhash160, VersionEncoding.VER_ACCOUNT_ID)
+        }
 
+        def asStrSeed = StrSeed.parse(rawstr).get
+        def asStrAddress = StrAddress.parse(rawstr).get
+      }
+    }
+
+    implicit def legacybridge(gkey :StrAddress) = new {
+      def legacy = address(gkey.byteBuffer)
+    }
+    implicit def legacybridge(skey :StrSeed) = new {
+      def legacy =seed(skey.rawseed)
+    }
   }
 }
 }
--- shared/src/test/scala/legacykey_tests.scala
+++ shared/src/test/scala/legacykey_tests.scala
@@ -2,31 +2,34 @@ package mytests
 
 import com.inthenow.zcheck.{SpecLite}
 
-import org.strllar.stellarbase.{StrSeed, StrKey}
-import org.strllar.stellarbase.legacy.{StrKey => LegacyStrKey}
+import org.strllar.stellarbase.StrAddress
+import org.strllar.stellarbase.legacy.StrKey.Converters._
 
 object LegacyStrKeySpec extends SpecLite {
 
-  def isPairedLegacyKey(skey :String, addr :String): Unit = {
-    val someseed = LegacyStrKey.decodeCheck(skey)
-    val pubkey = StrSeed(someseed toArray).address.byteBuffer
-    val someaddr = LegacyStrKey.address(pubkey)
-    someaddr must_== addr
-  }
+  "legacy keys" should {
+    "be identity with decode+encode" in {
+      "s3mH7MZ5ud38orpMFjXtUcu3jb3nhdQTPVC6Td4rVbi9biCm1W5".asLegacySeed.toString must_== "s3mH7MZ5ud38orpMFjXtUcu3jb3nhdQTPVC6Td4rVbi9biCm1W5"
+      "gggGpCWrMJwngLjeWqa27BtMTkqmwfMvt".asLegacyAddress.toString must_== "gggGpCWrMJwngLjeWqa27BtMTkqmwfMvt"
+    }
 
-  "sandbox here" in {
+    def checkIsPairedLegacyKey(skey :String, addr :String): Unit = {
+      skey.asLegacySeed.upgrade.address.legacy must_== addr
+    }
 
-    isPairedLegacyKey("s3mH7MZ5ud38orpMFjXtUcu3jb3nhdQTPVC6Td4rVbi9biCm1W5", "gMCUfsYgEQZnhCR3cKrxnwnuDho9emWUf8")
-    isPairedLegacyKey("sfBuEPPnEQ67whwf8tpwkJdCTiA6TpC1FG5Jgkq9numMZ63YcPw", "gggR6rawTsCo6tBHx5RwyfgzHXbNYc64Z")
-    isPairedLegacyKey("sfBuLU86ZEWLomNrVZZWY8xf342Z6m8MgJeWNxZyRhZe4JhBVg9", "ggg9mWaz4MTyK2YJ8pT4smQ6r32xrB7Z7")
-    isPairedLegacyKey("sfBuWo2ENDLJ4gpjCaHPMaKSpPLSJXW3D2KvxqNFdP8hMxvgmPy", "gggGpCWrMJwngLjeWqa27BtMTkqmwfMvt")
+    "should be paired" in {
+      checkIsPairedLegacyKey("s3mH7MZ5ud38orpMFjXtUcu3jb3nhdQTPVC6Td4rVbi9biCm1W5", "gMCUfsYgEQZnhCR3cKrxnwnuDho9emWUf8")
+      checkIsPairedLegacyKey("sfBuEPPnEQ67whwf8tpwkJdCTiA6TpC1FG5Jgkq9numMZ63YcPw", "gggR6rawTsCo6tBHx5RwyfgzHXbNYc64Z")
+      checkIsPairedLegacyKey("sfBuLU86ZEWLomNrVZZWY8xf342Z6m8MgJeWNxZyRhZe4JhBVg9", "ggg9mWaz4MTyK2YJ8pT4smQ6r32xrB7Z7")
+      checkIsPairedLegacyKey("sfBuWo2ENDLJ4gpjCaHPMaKSpPLSJXW3D2KvxqNFdP8hMxvgmPy", "gggGpCWrMJwngLjeWqa27BtMTkqmwfMvt")
+    }
 
-//    val seed = StrKey.random()
-//    val addr = seed.address
-//    println(seed)
-//    println(addr)
-//    println(LegacyStrKey.seed(seed.rawseed))
-//    println(LegacyStrKey.address(addr.byteBuffer))
+    "pass other tests" in {
+      val abc = "s3mH7MZ5ud38orpMFjXtUcu3jb3nhdQTPVC6Td4rVbi9biCm1W5".asLegacyAddress.mustThrowA[Exception]
+      "gggGpCWrMJwngLjeWqa27BtMTkqmwfMvt".asLegacySeed.mustThrowA[Exception]
 
+      "GAKIEJNSVB44WDEVNDY6RGRVOXWHZWYM7V3UULOZBVISONXM76OYHRGU" .asStrAddress.legacy must_== "gUtknaqjh8LMRedgFLof2E2LZuiEihQDVD"
+      "GB2Y4SUXOWSTXHTL7QVNGGK3Y6IOMUGHDB3ZICWJN2EJ2PJHORWS3LG4".asStrAddress.legacy  must_== "gNvsRBqV5YEarv7RCjuYzmsYoXDJpeHRtD"
+    }
   }
 }
